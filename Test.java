e1a1f9273a0e01f75a9b24f5f76800a61ad5007b
albertommj <albertommj@gmail.com>
2019-03-05 10:33:57 +0100

    Test.java

---
+++ Test.java
@@ -0,0 +1,13 @@
+package coleccionDiscos;
+
+class Test {
+
+	public static void main(String[] args) {
+		String[] opciones = {"Mostrar Discos","Añadir discos","Borrar discos","Salir"};
+		Coleccion[] coleccion = new Coleccion[10];
+		int opcion;
+		
+		opcion = coleccion.pintarMenu(opciones)
+	}
+
+}
