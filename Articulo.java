92e6020199d00ea41f31cab785652fbebfe23501
albertommj <albertommj@gmail.com>
2019-03-05 10:35:24 +0100

    Articulo.java

---
+++ Articulo.java
@@ -0,0 +1,47 @@
+package gestisimal;
+
+public class Articulo {
+	private String descripcion;
+	private float precioCompra;
+	private float precioVenta;
+	private int stock;
+	
+	Articulo(String descripcion,float precioCompra,float precioVenta,int stock) {
+		this.descripcion = descripcion;
+		this.precioCompra = precioCompra;
+		this.precioVenta = precioVenta;
+		this.stock = stock;
+	}
+
+	public String getDescripcion() {
+		return descripcion;
+	}
+
+	public void setDescripcion(String descripcion) {
+		this.descripcion = descripcion;
+	}
+
+	public float getPrecioCompra() {
+		return precioCompra;
+	}
+
+	public void setPrecioCompra(float precioCompra) {
+		this.precioCompra = precioCompra;
+	}
+
+	public float getPrecioVenta() {
+		return precioVenta;
+	}
+
+	public void setPrecioVenta(float precioVenta) {
+		this.precioVenta = precioVenta;
+	}
+
+	public int getStock() {
+		return stock;
+	}
+
+	public void setStock(int stock) {
+		this.stock = stock;
+	}
+}
