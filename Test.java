6cb519fc557766406ca0a0ec73cc1437a8dc5eb2
albertommj <albertommj@gmail.com>
2019-03-25 18:33:13 +0100

    Add files via upload

---
+++ Test.java
@@ -0,0 +1,14 @@
+package barajaEspanola;
+
+class Test {
+
+	public static void main(String[] args) {
+		Baraja baraja = new Baraja();
+		System.out.print(Baraja.extraerNCartas(baraja, 9));
+		
+	}
+
+	
+	
+
+}
