e83e1a9db6f77c36cd8e94206d247cb8c04613ab
albertommj <albertommj@gmail.com>
2019-05-23 09:47:12 +0200

    Add files via upload

---
+++ TestCuadrado.java
@@ -0,0 +1,13 @@
+package rectangulo;
+
+
+class TestCuadrado {
+
+  public static void main(String[] args) {
+   Cuadrado cuadrado = new Cuadrado(7);
+   cuadrado.toString();
+   Cuadrado cuadradoError = new Cuadrado(15);
+   cuadrado.toString();
+  }
+
+}
